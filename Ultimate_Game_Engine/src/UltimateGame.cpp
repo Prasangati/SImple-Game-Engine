67d8b43ab5a1cd263e245632763727200dc2cb1f
Prasangati <53667641+Prasangati@users.noreply.github.com>
2024-05-03 00:34:01 -0400

    finally. The window is opening

--- Ultimate_Game_Engine/src/UltimateGame.cpp
+++ Ultimate_Game_Engine/src/UltimateGame.cpp
@@ -3,10 +3,9 @@
 //
 #include "pch.h"
 #include "UltimateGame.h"
-
-#include <GL/gl.h>
-
 #include "UltimateWindow.h"
+#include "GLFW/glfw3.h"
+
 namespace Ultimate{
 
     void UltimateGame::Initialize() {
@@ -36,7 +35,8 @@ namespace Ultimate{
         /// Vertex Data ///
 
         unsigned int VBO, VAO;
-        glGenVertexArrays(1,&VAO);
+        glGenVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
 
         float vertices[] = {
             -0.5f, -0.5f, 0.0, 0.0,
@@ -62,6 +62,67 @@ namespace Ultimate{
 
         unsigned int EBO;
         glGenBuffers(1, &EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        const char* vertexShaderSource = R"(
+            #version 330 core
+            layout (location = 0) in vec2 aPos;
+            layout (location = 1) in vec2 aTexCoord;
+
+            void main()
+            {
+                gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);
+                TexCoord = aTexCoord;
+            }
+            )";
+
+        const char* fragmentShaderSource = R"(
+            #version 330 core
+            out vec4 FragColor;
+
+            in vec2 TexCoord;
+
+            uniform sampler2D myTex;
+
+            void main()
+            {
+                FragColor = texture(myTex, TexCoord);
+            }
+            )";
+        unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+        glCompileShader(vertexShader);
+        int success;
+        char infoLog[512];
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+            ULTIMATE_ERROR("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog);
+        }
+
+        unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+        glCompileShader(fragmentShader);
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+            ULTIMATE_ERROR("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog);
+        }
+
+        unsigned int shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+// check for linking errors
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+
+
+
+
 
 
 
